cb2c656dda69a9665176b27062d5c8fdae39e8c9
bortolilucas <lucasbortolidev@gmail.com>
2023-06-28 21:31:21 -0300

    feat: add metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { montserratFont } from "@/assets/fonts";
 
@@ -7,6 +8,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <div
       className={`min-h-[100dvh] flex bg-white flex-col ${montserratFont.className} text-dark font-medium`}
     >
+      <Head>
+        <title>Doses de Amor Próprio - Gabriela Pita</title>
+        <meta
+          name="description"
+          content="O impulso que você precisa para se tornar a líder da própria vida"
+        />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
