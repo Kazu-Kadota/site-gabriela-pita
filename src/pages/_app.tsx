fda351bc418c3feac9e45d8b41b205bcef6e34c0
bortolilucas <lucasbortolidev@gmail.com>
2023-06-30 11:19:58 -0300

    feat: improve seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,19 @@
 import "@/styles/globals.css";
-import Head from "next/head";
-import type { AppProps } from "next/app";
 import { montserratFont } from "@/assets/fonts";
+import { NextSeo } from "next-seo";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
       className={`min-h-[100dvh] flex bg-white flex-col ${montserratFont.className} text-dark font-medium`}
     >
-      <Head>
-        <title>Doses de Amor Próprio - Gabriela Pita</title>
-        <meta
-          name="description"
-          content="O impulso que você precisa para se tornar a líder da própria vida"
-        />
-      </Head>
+      <NextSeo
+        title="Doses de Amor Próprio - Gabriela Pita"
+        description="O impulso que você precisa para se tornar a líder da própria vida"
+        robotsProps={{}}
+        canonical="https://www.gabrielapita.com/"
+      />
       <Component {...pageProps} />
     </div>
   );
